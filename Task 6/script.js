3c1c7c44d9b1441c193da98e6d7f43a045d15f04
VitaZil <vita.bun@gmail.com>
2022-07-09 13:52:45 +0300

    6 uzduotis

--- Task 6/script.js
+++ Task 6/script.js
@@ -9,13 +9,28 @@ atveju grąžins visų "users" vardus naujame masyve pvz., ['John Smith', 'Ann S
 -------------------------------------------------------------------------- */
 
 const users = [
-  { id: '1', name: 'John Smith', age: 20 },
-  { id: '2', name: 'Ann Smith', age: 24 },
-  { id: '3', name: 'Tom Jones', age: 31 },
-  { id: '4', name: 'Rose Peterson', age: 17 },
-  { id: '5', name: 'Alex John', age: 25 },
-  { id: '6', name: 'Ronald Jones', age: 63 },
-  { id: '7', name: 'Elton Smith', age: 16 },
-  { id: '8', name: 'Simon Peterson', age: 30 },
-  { id: '9', name: 'Daniel Cane', age: 51 },
+  { id: "1", name: "John Smith", age: 20 },
+  { id: "2", name: "Ann Smith", age: 24 },
+  { id: "3", name: "Tom Jones", age: 31 },
+  { id: "4", name: "Rose Peterson", age: 17 },
+  { id: "5", name: "Alex John", age: 25 },
+  { id: "6", name: "Ronald Jones", age: 63 },
+  { id: "7", name: "Elton Smith", age: 16 },
+  { id: "8", name: "Simon Peterson", age: 30 },
+  { id: "9", name: "Daniel Cane", age: 51 },
 ];
+
+const getUserAverageAge = (array) => {
+  const ageValues = array.map((item) => item.age);
+  const sum = ageValues.reduce((a, b) => a + b);
+  const ageAverage = sum / ageValues.length;
+  return ageAverage;
+};
+
+console.log(getUserAverageAge(users));
+
+const getUsersNames = (array) => {
+  return array.map((item) => item.name);
+};
+
+console.log(getUsersNames(users));
